b212555a8fa8e2aba9d4ffd26f9b656c44dc4eb3
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2024-08-30 23:40:06 +0200

    make sure program fails on missing inputs

--- src/program.test.ts
+++ src/program.test.ts
@@ -1,5 +1,5 @@
 import { Effect } from 'effect'
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 import { program } from './program'
 import { runMainTest } from './run-main'
 import { getConfigProviderTest } from './services/config-provider'
@@ -13,9 +13,26 @@ test('runs without errors', () => {
                 'workflow-file-name':
                     'octocat/hello-world/.github/workflows/my-workflow.yml@refs/heads/my_branch',
                 'workflow-job-name': 'my-job',
-                'dry-run': false,
+                'dry-run': true,
             }),
         ),
         runMainTest,
     )
+})
+
+test('throws an error on missing inputs', () => {
+    expect(() =>
+        program.pipe(
+            Effect.provide(
+                getConfigProviderTest({
+                    'github-token': 'test-token',
+                    'deployed-commit-sha': 'ffac537e6cbbf934b08745a378932722df287a53',
+                    'workflow-file-name':
+                        'octocat/hello-world/.github/workflows/my-workflow.yml@refs/heads/my_branch',
+                    'workflow-job-name': 'my-job',
+                }),
+            ),
+            Effect.runSync,
+        ),
+    ).toThrowError('There was an error with an input.')
 })
