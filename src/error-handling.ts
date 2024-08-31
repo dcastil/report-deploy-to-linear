bf797b2da32939af6b4c2156527cf496250079ca
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2024-08-31 13:39:36 +0200

    rename ExitError to FatalError

--- src/error-handling.ts
+++ src/error-handling.ts
@@ -1,14 +1,14 @@
-interface ExitErrorParams {
+interface FatalErrorParams {
     title: string
     messages?: readonly any[]
 }
 
-export class ExitError extends Error {
+export class FatalError extends Error {
     readonly title: string
     readonly messages: readonly any[]
 
-    constructor(params: ExitErrorParams) {
-        super(params.title)
+    constructor(params: FatalErrorParams) {
+        super([params.title, ...(params.messages ?? [])].join('\n    '))
         this.title = params.title
         this.messages = params.messages ?? []
     }
--- src/run-main.ts
+++ src/run-main.ts
@@ -1,12 +1,12 @@
 import { NodeRuntime } from '@effect/platform-node'
 import { Cause, Effect, Exit } from 'effect'
-import { ExitError } from './error-handling'
+import { FatalError } from './error-handling'
 
-export function runMainLive<A>(effect: Effect.Effect<A, ExitError>) {
+export function runMainLive<A>(effect: Effect.Effect<A, FatalError>) {
     return NodeRuntime.runMain(effect, { disableErrorReporting: true })
 }
 
-export function runMainTest<A>(effect: Effect.Effect<A, ExitError>) {
+export function runMainTest<A>(effect: Effect.Effect<A, FatalError>) {
     return NodeRuntime.runMain(effect, {
         disableErrorReporting: true,
         teardown: (exit) => {
